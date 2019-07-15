c4f7378ddadff1e124f3cb60ceca4cd3f71a5d6c
evd3v@yandex.ru <evd3v@yandex.ru>
2019-07-15 17:49:32 +0300

    final version 1.0 beta

--- js/main.js
+++ js/main.js
@@ -5,37 +5,105 @@ window.onload = function() {
     const IMAGES = 3;
 
     let sliderDots = document.querySelectorAll('.header .nav-slider li');
-    
     let sliderControl = document.querySelector('.header .nav-slider');
 
-    setInterval(function() {
-        bg.classList.remove('bg-' + count);
-        sliderDots[count].classList.remove('active');
-        if(count === 2) {
-            count = 0;
-            bg.classList.add('bg-' + count);
-            sliderDots[count].classList.add('active');
-            return true;
-        }
-        count++;
-        bg.classList.add('bg-' + count);
-        sliderDots[count].classList.add('active');
-    }, 5000);
+    let modalWindow = document.querySelector('.modal-window');
+    let modalOverlay = document.querySelector('.modal-overlay');
+
+    let navBar = document.querySelector('.navbar');
+    let close = document.querySelector('.close');
+
+    let sliderOn = setInterval(slider, 5000);
+
+    let unFadeId,
+    fadeId;
 
     sliderControl.onclick = function(event) {
+        
         let currentItem = event.target;
         let currentCount = +currentItem.id[4];
 
         if(isNaN(+currentItem.id[4])) {
             return 0;
         }
+        clearInterval(sliderOn);
+        bg.classList.remove('bg-' + count);
+        sliderDots[count].classList.remove('active');
         count = currentCount;
-        for(let i = 0; i < 3; i++) {
-            sliderDots[i].classList.remove('active');
-            bg.classList.remove('bg-' + i);
-        }
         console.log(count);
         sliderDots[count].classList.add('active');
         bg.classList.add('bg-' + count);
+        sliderOn = setInterval(slider, 5000);
     };
+
+    function slider() {
+        bg.classList.remove('bg-' + count);
+        sliderDots[count].classList.remove('active');
+        if(count === 2) {
+           count = 0;
+           bg.classList.add('bg-' + count);
+           sliderDots[count].classList.add('active');
+           return true;
+        }
+        count++;
+        bg.classList.add('bg-' + count);
+        sliderDots[count].classList.add('active');
+    }
+
+    function showModal() {
+        modalWindow.style.display = 'block';
+        modalOverlay.style.display = 'block';
+        modalOverlay.style.opacity = 0;
+        modalWindow.style.opacity = 0;
+        let modalUnFade = setInterval(unFade, 50);
+        unFadeId = modalUnFade;
+    }
+
+    function hideModal() {
+        let modalFade = setInterval(fade, 50);
+        fadeId = modalFade;
+    }
+
+    navBar.onclick = function(event) {
+        showModal();
+        document.body.style.overflow = 'hidden';
+    };
+
+    modalOverlay.onclick = function(event) {
+        hideModal();
+    };
+
+    close.onclick = function() {
+        modalWindow.style.display = 'none';
+        modalOverlay.style.display = 'none';
+        modalOverlay.style.opacity = '0';
+        modalWindow.style.opacity = '0';
+        document.body.style.overflow = 'auto';
+    };
+
+    function fade() {
+        if(modalOverlay.style.opacity != 0) {
+            console.log(modalWindow.style.opacity);
+            modalWindow.style.opacity = +modalWindow.style.opacity - 0.2;
+            modalOverlay.style.opacity = +modalOverlay.style.opacity - 0.1;
+        } else {
+            setTimeout(function() {
+                modalWindow.style.display = 'none';
+                modalOverlay.style.display = 'none';
+                document.body.style.overflow = 'auto';
+            }, 250);
+            clearInterval(fadeId);
+        }
+    }
+    
+    function unFade() {
+        if(modalOverlay.style.opacity < 0.55) {
+            console.log(modalWindow.style.opacity);
+            modalWindow.style.opacity = +modalWindow.style.opacity + 0.2;
+            modalOverlay.style.opacity = +modalOverlay.style.opacity + 0.1;
+        } else {
+            clearInterval(unFadeId);
+        }
+    }
+
 };
